a8c15c956963ed658bb27f21b332bd8c55a9eacd
99Alphapriority <prajneets@gmail.com>
2024-03-04 00:56:40 -0700

    question 90 solution and explanation.

---
+++ code/Leetcode/subsets_2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void dfs(vector<int>& nums, int start, vector<int>& curr, vector<vector<int>>& ans)
+    {
+        ans.push_back(curr);
+
+        for(int i = start; i < nums.size(); i++)
+        {
+            if(i > start && nums[i] == nums[i-1])
+                continue;
+
+            curr.push_back(nums[i]);
+            dfs(nums, i + 1, curr, ans);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        sort(nums.begin(), nums.end());
+        dfs(nums, 0, curr, ans);
+        return ans;
+    }
+};
